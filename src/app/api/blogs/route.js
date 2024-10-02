8fffe0fbd66c33fd37f322180911bc78984d1d58
Ujjwal <bansalujjwal2002@gmail.com>
2024-10-02 22:20:50 +0530

    blog dynamic

--- src/app/api/blogs/route.js
+++ src/app/api/blogs/route.js
@@ -1,5 +1,7 @@
 import { NextResponse } from 'next/server';
 import * as fs from 'fs/promises';
+export const dynamic = 'force-dynamic';
+
 
 export async function GET(request) {
     const data = await fs.readdir("blogdata","utf-8")
